00b7931afcec67b4c878c09caf670792dfc3fa3e
neednoname09 <mandyana@usc.edu>
2024-12-20 16:19:51 -0800

    Program flow description

--- csi_analysis/training/channel_classifier/channel_classifier/dataset.py
+++ csi_analysis/training/channel_classifier/channel_classifier/dataset.py
@@ -9,6 +9,8 @@ from channel_classifier.config import (
     DATA_DIR, RAW_DATA_DIR, INTERIM_DATA_DIR, PROCESSED_DATA_DIR,
     EXTERNAL_DATA_DIR,
 )
+from csi_images.csi_events import EventArray
+from csi_utils import csi_databases
 
 def main(
         raw_data: Path = RAW_DATA_DIR,
@@ -17,6 +19,14 @@ def main(
         external_data: Path = EXTERNAL_DATA_DIR,
 ):
     logger.info(f"DATA_DIR path is: {DATA_DIR}")
+
+    # Query the analysis table from prod database to get the list of
+    # locked slides.
+    slides = get_locked_slides()
+
+    # query identifiers forthe events from ocular_hitlist from all
+    # the locked slides(slide_id, frame_id, x, y)
+
 
 
 if __name__ == "__main__":
